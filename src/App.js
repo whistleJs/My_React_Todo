b12c65174756425913b5e8822f0aa0036a80722c
whistleJs <whistle_js@naver.com>
2020-09-09 23:32:40 +0900

    2020-09-09 Main.jsx 작업

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import TodoMain from './todo/main';
+
 function App() {
   return (
-    <div>
+    <div className="flex align-items-center justify-content-center app">
+      <TodoMain />
     </div>
   );
 }
---
+++ src/todo/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './index.css';
+
+function TodoMain() {
+  return (
+    <div className="main flex column">
+    </div>
+  )
+}
+
+export default TodoMain;
