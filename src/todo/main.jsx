b42730eae6b9f0dc9929406a5e7cb1658c64a1af
whistleJs <whistle_js@naver.com>
2020-09-15 00:02:44 +0900

    2020-09-15 main.jsx 작업 (list.jsx 적용)

--- src/todo/main.jsx
+++ src/todo/main.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import _ from 'lodash';
+
+import React, { useState } from 'react';
 
 import Header from './header';
+import List from './list';
 
 import './index.css';
 
 function TodoMain() {
+  const [todoList, setTodoList] = useState([{ id: 1, name: 'test', isComplete: false }, { id: 2, name: 'test2', isComplete: true }]);
+
+  // Toggle Complete Todo Item
+  const onToggleComplete = (index) => {
+    let list = _.cloneDeep(todoList);
+    list[index].isComplete = !list[index].isComplete;
+
+    setTodoList(list);
+  }
+
   return (
     <div className="main flex column">
       <Header />
+
+      <List
+        list={todoList}
+        onToggleComplete={onToggleComplete}
+      />
     </div>
   )
 }
