459333e3af259253fe659e3ab8791e9fece6b3bc
whistleJs <whistle_js@naver.com>
2020-09-15 22:05:52 +0900

    2020-09-15 main.jsx - footer.jsx 연결 작업

--- src/todo/main.jsx
+++ src/todo/main.jsx
@@ -4,11 +4,27 @@ import React, { useState } from 'react';
 
 import Header from './header';
 import List from './list';
+import Footer from './footer';
 
 import './index.css';
 
 function TodoMain() {
   const [todoList, setTodoList] = useState([{ id: 1, name: 'test', isComplete: false }, { id: 2, name: 'test2', isComplete: true }]);
+
+  // Create Todo Item
+  const onCreateTodoItem = (todoName) => {
+    if (todoName === '') return;
+
+    let find = todoList.find(todoItem => todoItem.name === todoName);
+
+    if (find) return;
+
+    const list = [...todoList, { name: todoName, isComplete: false }];
+
+    setTodoList(list);
+
+    alert('[알림] 성공적으로 할 일을 추가하였습니다.');
+  }
 
   // Toggle Complete Todo Item
   const onToggleComplete = (index) => {
@@ -25,6 +41,10 @@ function TodoMain() {
       <List
         list={todoList}
         onToggleComplete={onToggleComplete}
+      />
+
+      <Footer
+        onCreate={onCreateTodoItem}
       />
     </div>
   )
